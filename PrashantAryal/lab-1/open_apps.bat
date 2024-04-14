2a238874c6f952a3ae23eb250684381a3321b0ec
PrashantAryal <81633904+PrashantAryal@users.noreply.github.com>
2024-04-14 20:56:59 +0545

    DN lab 1 and 2

---
+++ PrashantAryal/lab-1/open_apps.bat
@@ -0,0 +1,20 @@
+@echo off
+
+REM Prompt user to enter a URL
+set /p url="Enter URL for opening in Google Chrome: "
+
+REM Open Google Chrome with the specified URL
+start "C:\Program Files\Google\Chrome\Application\chrome.exe" "%url%"
+
+
+REM Wait for 3 seconds before opening the next application
+timeout /t 3 >nul
+
+REM Open Visual Studio Code
+start "C:\Users\user\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+
+REM Wait for 2 seconds before opening the next application
+timeout /t 2 >nul
+
+REM Open SQL Server Management Studio (SSMS)
+start "C:\Program Files (x86)\Microsoft SQL Server Management Studio 19\Common7\IDE\Ssms.exe"
